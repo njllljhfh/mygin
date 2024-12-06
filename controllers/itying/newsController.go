5e851a1214a0d6a986972b53fddc13092c845d77
njl <niujinlong@matrixtime.com>
2024-12-06 18:26:41 +0800

    中间件

--- controllers/itying/newsController.go
+++ controllers/itying/newsController.go
@@ -1,22 +1,24 @@
 package itying
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "net/http"
 )
 
 type NewsController struct{}
 
 // News 前台新闻
 func (con *NewsController) News(c *gin.Context) {
-	news := &Article{
-		Title:   "新闻-标题",
-		Desc:    "新闻-描述",
-		Content: "新闻-详情",
-	}
-	// 返回html模板
-	c.HTML(http.StatusOK, "default/news.html", gin.H{
-		"title": "新闻页面",
-		"news":  news,
-	})
+    fmt.Printf("这是一个新闻页面\n")
+    news := &Article{
+        Title:   "新闻-标题",
+        Desc:    "新闻-描述",
+        Content: "新闻-详情",
+    }
+    // 返回html模板
+    c.HTML(http.StatusOK, "default/news.html", gin.H{
+        "title": "新闻页面",
+        "news":  news,
+    })
 }
--- routers/defaultRouters.go
+++ routers/defaultRouters.go
@@ -1,17 +1,29 @@
 package routers
 
 import (
-	"github.com/gin-gonic/gin"
-	"mygin/controllers/itying"
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "mygin/controllers/itying"
 )
 
-func DefaultRouterInit(r *gin.Engine) {
-	defaultRouters := r.Group("/")
-	{
-		indexController := &itying.IndexController{}
-		defaultRouters.GET("/", indexController.Index)
+func initMiddleware(c *gin.Context) {
+    fmt.Println("中间件: 执行 处理函数 前 --- 1")
+    // 调用该请求的剩余处理程序
+    c.Next()
+    fmt.Println("中间件: 执行 处理函数 前 --- 2")
+}
 
-		newsController := &itying.NewsController{}
-		defaultRouters.GET("/news", newsController.News)
-	}
+/*
+web 的 index 接口
+*/
+
+func DefaultRouterInit(r *gin.Engine) {
+    defaultRouters := r.Group("/")
+    {
+        indexController := &itying.IndexController{}
+        defaultRouters.GET("/", initMiddleware, indexController.Index)
+
+        newsController := &itying.NewsController{}
+        defaultRouters.GET("/news", initMiddleware, newsController.News)
+    }
 }
